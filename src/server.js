2558923a68a116e8b1005b6c9d759f22cdebcac4
rafaelsofizadeh <rafa.sofizadeh@gmail.com>
2019-08-28 17:18:04 +0400

    Single tick

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
-const express = require("express");
-require('dotenv').config()
-require("./db/mongoose");
+const express = require('express');
+require('dotenv').config();
+require('./db/mongoose');
 
 const app = express();
 const port = process.env.PORT || 3000;
