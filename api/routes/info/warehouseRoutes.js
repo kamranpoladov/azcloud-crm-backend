2f359b120e9b6e8b35b37d8df0bad58e5e30b951
rafaelsofizadeh <rafa.sofizadeh@gmail.com>
2019-08-30 07:12:08 +0400

    Эй сучка

--- api/routes/info/warehouseRoutes.js
+++ api/routes/info/warehouseRoutes.js
@@ -1,4 +1,28 @@
 const express = require('express');
 const router = new express.Router();
 
+const Cluster = require('../../../db/models/warehouse/clusterModel');
+
+router.get('/', (request, response, next) => {
+    const clusters = await Cluster
+        .find({})
+        .populate('children');
+
+    const result = clusters.map(cluster => {
+        const totalProcessingPower = cluster.totalProcessingPower;
+        const totalUsedProcessingPower = cluster.children.reduce((accumulator, vs) => {
+            const vsClockSpeed = vs.getClockSpeedAndStorage().clockSpeed;
+            accumulator += vsClockSpeed * vs.cores;
+        });
+
+        return {
+            ...cluster._doc,
+            totalProcessingPower,
+            totalUsedProcessingPower
+        };
+    });
+
+    response.status(200).send(result);
+});
+
 module.exports = router;
